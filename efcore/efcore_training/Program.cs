d25a5930d195acc08000fc233a925371edbadad4
KatlameyArtem <you@gmail.com>
2025-09-28 14:08:20 +0300

    deleting eneteties

--- efcore/efcore_training/Program.cs
+++ efcore/efcore_training/Program.cs
@@ -16,18 +16,43 @@ namespace efcore_training
             //GettinOneProduct();
             //QueryingWithLike();
             //GetRandomProduct();
+
+
+
+
             //var resultadd = AddProducts(6, "Bob`s Burfers", 500M, 72);
             //if(resultadd.affected == 1)
             //{
             //    WriteLine($"Add product successful with ID:{resultadd.productId}");
             //}
             //ListProducts(productIdsToHightlight: new[] { resultadd.productId });
-            var resultUpdate = IncreaseProductPrise("Bob", 20M);
-            if(resultUpdate.affected == 1)
+
+
+
+
+
+
+            //var resultUpdate = IncreaseProductPrise("Bob", 20M);
+            //if(resultUpdate.affected == 1)
+            //{
+            //    WriteLine($"Increase price success for ID: {resultUpdate.productId}");
+            //}
+            //ListProducts(productIdsToHightlight: new[] {resultUpdate.productId });
+
+
+
+
+            WriteLine("About to delete all products whose name starts with Bob.");
+            Write("Press enter to continue or any key to exit: ");
+            if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
             {
-                WriteLine($"Increase price success for ID: {resultUpdate.productId}");
+                int deleted = DeleteProducts("Bob");
+                WriteLine($"{deleted} product(s) were deleted");
             }
-            ListProducts(productIdsToHightlight: new[] {resultUpdate.productId });
+            else
+            {
+                WriteLine("Delete was canceled");
+            }
         }
     }
 }
